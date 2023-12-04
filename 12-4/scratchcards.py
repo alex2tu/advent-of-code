6e9732a56bbd4e347732238874e1e088c113b10e
alex2tu <alexandertu0215@gmail.com>
2023-12-04 00:46:03 -0500

    day 4 answers

---
+++ 12-4/scratchcards.py
@@ -0,0 +1,41 @@
+res = 0
+
+with open("12-4/input.txt", "r") as f:
+    line = f.readline()
+
+    while line != "":
+
+        scratchcardSplit = line.split(":")
+        numbers = scratchcardSplit[1].strip().split("|")
+        winningNumbers = numbers[0].split(" ")
+        yourNumbers = numbers[1].split(" ")
+
+        i = 0
+        while i < 10:
+            if winningNumbers[i] == "" or winningNumbers[i] == " ":
+                winningNumbers.remove("")
+            else:
+                i += 1
+        if winningNumbers[-1] == "":
+            winningNumbers = winningNumbers[:-1]
+        j = 0
+        while j < 25:
+            if yourNumbers[j] == "" or yourNumbers[j] == " ":
+                yourNumbers.remove("")
+            else:
+                j += 1
+        
+        winningSet = set(winningNumbers)
+        yourSet = set(yourNumbers)
+
+        currEx = 0
+        for num in yourSet:
+            if num in winningSet:
+                currEx += 1
+
+        if currEx:
+            res += 2**(currEx-1)
+
+        line = f.readline()
+
+print(res)
