34912f129abb4e5a6ec9210fcc9a5dc58d24cd83
alex2tu <alexandertu0215@gmail.com>
2023-12-06 00:27:39 -0500

    boilerplate code

---
+++ boilerplatecode.py
@@ -0,0 +1,20 @@
+with open("12-0/input.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+def partOneSolve(lines):
+    res = 0
+    for line in lines:
+        print("blah")
+    
+    return res
+
+# print(partOneSolve(lines))
+
+def partTwoSolve(lines):
+    res = 0
+    for line in lines:
+        print("bleh")
+    
+    return res
+
+# print(partTwoSolve(lines))
